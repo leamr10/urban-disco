955960d7a45430a6b612fa07567f1380a8cef76f
leamr10 <leamr10@gmail.com>
2023-08-14 18:58:47 -0400

    post js edited

--- public/js/post.js
+++ public/js/post.js
@@ -21,3 +21,28 @@ const newPostHandler = async (event) => {
         alert('Failed to create post');
       };
   
+const updatedPostHandler = async(event) => {
+    event.preventDefault();
+
+    const updatedPost = event.target.id;
+    const name = event.target[0].value;
+    const text = event.target[1].value;
+
+    if (updatedPost)
+    {
+        if (name || text)
+        {
+            await fetch(`/api/dashboard/${updatedPost}`, {
+                method: 'PUT',
+                body: JSON.stringify({name, text}),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((res) => res.json())
+      .then((data) => { 
+        document.location.reload()
+      })
+    }
+        alert('Failed to update post');
+}};
